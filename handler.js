2cc91ff67917f1a2c7293699df83995381eb0108
NathanAlcantara <nathangabriel97@gmail.com>
2019-09-23 21:52:58 -0300

    First commit

---
+++ handler.js
@@ -0,0 +1,12 @@
+module.exports.helloFellows = async (event) => {
+  var name = "GAIA"
+
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name;
+  }
+
+  return {
+    statusCode: '200',
+    body: `Hello my fellows, i'm ${name}}`
+  };
+};
